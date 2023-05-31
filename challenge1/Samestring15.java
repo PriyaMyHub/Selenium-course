60cb5f67adc05ca6c3fc8eaee7eb2a0f970949f6
PriyaMyHub <132281002+PriyaMyHub@users.noreply.github.com>
2023-05-31 12:22:12 +0530

    Add files via upload

---
+++ challenge1/Samestring15.java
@@ -0,0 +1,37 @@
+package challenge1;
+
+public class Samestring15 {
+	
+	public static void main(String[] args) {
+		
+		String[] word1= {"ab","c"};
+		String[] word2= {"a","bc"};
+		
+		String s1=String.join("",word1);
+		String s2=String.join("", word2);
+		
+		
+			System.out.println(s1.equals(s2));
+		}
+		
+		
+	}
+		
+	
+   
+	
+		
+	
+
+	
+	
+
+	
+			
+			
+		
+		
+
+	
+
+
